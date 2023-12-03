744842601057376ba6e838a1835ec7b5bb154696
Beadle <68292813+Beadle85@users.noreply.github.com>
2023-12-02 21:24:40 -0600

    migrate from glob pattern to content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
   return rss({
     title: 'Beadle | Blog',
     description: 'My journey learning Astro and more',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
